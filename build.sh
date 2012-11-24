7c57512d0eadc1bad08304c06f4105e3c66d0714
Sean Kelleher <ezanmoto@gmail.com>
2012-11-24 19:45:23 +0000

    Separated build and commit scripts

---
+++ build.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# This script runs checks, builds and tests.
+
+# This script should not be duplicated for running on specific platforms or
+# written in a more robust language like perl or python. Shell script has enough
+# features to achieve the desired operations and is supported or can be emulated
+# on multiple platforms, which makes it as good as an interpreted programming
+# language for the purpose of this script.
+
+# Extra build checks
+go tool vet src
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# Build
+go install bake
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# Run tests
+for PROJ in bake tests/perm
+do
+	go test $PROJ
+	if [ $? -ne 0 ]
+	then
+	    exit 1
+	fi
+done
+
+# Check formatting
+FMT=$(gofmt -d -s src)
+if [ "$FMT" != "" ]
+then
+    "$FMT" | tr \n \\n | less
+
+    APPLY=
+    while [ "$APPLY" = "" ]
+    do
+        echo "Apply changes?[y/n/q]"
+        read APPLY
+        case $APPLY in
+            y) gofmt -s -w src ;;
+            n) ;;
+            q) exit 0 ;;
+            *) APPLY=;;
+        esac
+    done
+fi
--- commit.sh
+++ commit.sh
@@ -8,49 +8,7 @@
 # on multiple platforms, which makes it as good as an interpreted programming
 # language for the purpose of this script.
 
-# Extra build checks
-go tool vet src
-if [ $? -ne 0 ]
-then
-    exit 1
-fi
-
-# Build
-go install bake
-if [ $? -ne 0 ]
-then
-    exit 1
-fi
-
-# Run tests
-for PROJ in bake tests/perm
-do
-	go test $PROJ
-	if [ $? -ne 0 ]
-	then
-	    exit 1
-	fi
-done
-
-# Check formatting
-FMT=$(gofmt -d -s src)
-if [ "$FMT" != "" ]
-then
-    "$FMT" | tr \n \\n | less
-
-    APPLY=
-    while [ "$APPLY" = "" ]
-    do
-        echo "Apply changes?[y/n/q]"
-        read APPLY
-        case $APPLY in
-            y) gofmt -s -w src ;;
-            n) ;;
-            q) exit 0 ;;
-            *) APPLY=;;
-        esac
-    done
-fi
+./build.sh
 
 # Commit
 if [ $# -ne 1 ]
