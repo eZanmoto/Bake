fbb7c37c2048bf39d87f7f6d9ba91aeadbe82154
Sean Kelleher <ezanmoto@gmail.com>
2012-12-27 16:22:28 -0800

    Replaced spaces with tabs in build script

--- build.sh
+++ build.sh
@@ -12,27 +12,27 @@
 go clean
 if [ $? -ne 0 ]
 then
-    exit 1
+	exit 1
 fi
 
 # Extra build checks
 go tool vet src
 if [ $? -ne 0 ]
 then
-    exit 1
+	exit 1
 fi
 
 # Build
 go install bake
 if [ $? -ne 0 ]
 then
-    exit 1
+	exit 1
 fi
 
 go install fmtincl
 if [ $? -ne 0 ]
 then
-    exit 1
+	exit 1
 fi
 
 # Run tests
@@ -42,7 +42,7 @@ do
 	go test $PROJ
 	if [ $? -ne 0 ]
 	then
-	    exit 1
+		exit 1
 	fi
 done
 
@@ -50,55 +50,55 @@ done
 FMT=$(gofmt -d -s src)
 if [ "$FMT" != "" ]
 then
-    "$FMT" | tr \n \\n | less
+	"$FMT" | tr \n \\n | less
 
-    APPLY=
-    while [ "$APPLY" = "" ]
-    do
-        echo "Apply changes?[y/n/q]"
-        read APPLY
-        case $APPLY in
-            y) gofmt -s -w src ;;
-            n) ;;
-            q) exit 0 ;;
-            *) APPLY=;;
-        esac
-    done
+	APPLY=
+	while [ "$APPLY" = "" ]
+	do
+		echo "Apply changes?[y/n/q]"
+		read APPLY
+		case $APPLY in
+			y) gofmt -s -w src ;;
+			n) ;;
+			q) exit 0 ;;
+			*) APPLY=;;
+		esac
+	done
 fi
 
 for LANG in templates/*
 do
-    if [ -f $LANG/*.fmt ]
-    then
-        rm $LANG/*.fmt
-    fi
+	if [ -f $LANG/*.fmt ]
+	then
+		rm $LANG/*.fmt
+	fi
 
-    for INCL in $LANG/*
-    do
-        if [ $(basename $INCL) != "{ProjectName}" ]
-        then
-            head -1 $INCL > $INCL.fmt
+	for INCL in $LANG/*
+	do
+		if [ $(basename $INCL) != "{ProjectName}" ]
+		then
+			head -1 $INCL > $INCL.fmt
 
-            DESCR=$(cat $INCL.fmt)
-            if [ "$DESCR" = "" -a $(basename $INCL) != "base" ]
-            then
-                echo "File description for '$INCL' is empty"
-                exit 1
-            elif [ ${#DESCR} -gt 50 ]
-            then
-                echo "File description for '$INCL' is over 50 chars (${#DESCR})"
-                exit 1
-            fi
+			DESCR=$(cat $INCL.fmt)
+			if [ "$DESCR" = "" -a $(basename $INCL) != "base" ]
+			then
+				echo "File description for '$INCL' is empty"
+				exit 1
+			elif [ ${#DESCR} -gt 50 ]
+			then
+				echo "File description for '$INCL' is over 50 chars (${#DESCR})"
+				exit 1
+			fi
 
-            fmtincl $INCL >> $INCL.fmt
-            diff $INCL $INCL.fmt >/dev/null
-            if [ $? -ne 0 ]
-            then
-                echo "Formatting '$INCL'..."
-                mv $INCL.fmt $INCL
-            else
-                rm $INCL.fmt
-            fi
-        fi
-    done
+			fmtincl $INCL >> $INCL.fmt
+			diff $INCL $INCL.fmt >/dev/null
+			if [ $? -ne 0 ]
+			then
+				echo "Formatting '$INCL'..."
+				mv $INCL.fmt $INCL
+			else
+				rm $INCL.fmt
+			fi
+		fi
+	done
 done
