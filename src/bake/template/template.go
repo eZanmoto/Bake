a82b5bc78eb4db7be844a68f30d2f9d682801562
Sean Kelleher <ezanmoto@gmail.com>
2014-01-20 09:28:08 +0000

    Added template package description

--- src/bake/template/template.go
+++ src/bake/template/template.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a GPL
 // license that can be found in the LICENSE file.
 
+// Package template provides an implementation of the bake template language.
 package template
 
 import (
